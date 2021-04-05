b625ae8fbb90d0081ff6c8e7bcaf486daee8b39a
Gereon Fox <ger.fox07@t-online.de>
2021-04-05 22:30:51 +0200

    Create a script that I want to make run with the new formalism.

--- examples/scratch4.py
+++ examples/scratch4.py
@@ -9,12 +9,38 @@ if __name__ == '__main__':
     hello = canvas.add(Text("Hello", cx=0.5, cy=0.25, visible=False))
     world = canvas.add(Text("World", cx=0.5, cy=0.75, visible=False))
 
-    def animate():
-        canvas.waitfor(next)
-        hello.visible = True
-        canvas.waitfor(next)
-        world.visible = True
+    # TODO: We specify the behavior of processes by giving some procedures.
+    #       These procedures are "executed statically", i.e. in some mode
+    #       that allows us to construct state graphs. The processes recognize this mode
+    #       and cooperate with the state graph construction.
+    #       That's how, for example, property assignments do not perform the actual computation, but just
+    #       record the action of updating the property value.
 
-    canvas.behavior = animate
+    def appear_smoothly(context, obj):
+        def a():
+            for _ in range(10):
+                context.wait(0.1)
+                obj.visible += 0.1
+
+        context.spawn(a)
+
+    def unfold(context):
+        context.waitfor(step)
+        appear_smoothly(context, hello)
+        context.waitfor(step)
+        appear_smoothly(context, world)
+
+    def bounce(context):
+        delta = context.var(0.01)
+        while True:
+            for _ in range(10):
+                context.wait(0.1)
+                hello.cy += delta
+                world.cy += delta
+            delta = - delta
+
+    canvas.behavior = concurrent(unfold, bounce)
 
     present(canvas)
+
+    # TODO: Alternatively it should be possible to first "unroll" the canvas process and *then* present it.
