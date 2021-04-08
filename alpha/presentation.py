ed2cd8c08b03da85bff8bf151319e959448bf40f
Gereon Fox <ger.fox07@t-online.de>
2021-04-08 22:55:40 +0200

    Defined Action.

--- alpha/presentation.py
+++ alpha/presentation.py
@@ -1,10 +1,46 @@
+from core.process import *
+from enum import Enum
+from alpha.behavior import *
 
-class Action:
+
+class Action(Interaction):
     """
     A way of interacting with a presentation.
     """
-    # TODO: Implement Action
-    pass
+
+    class Label(Enum):
+        """
+        Specifies the type of interaction that an Action object represents.
+        """
+        FORWARD = 1
+        ESCAPE = 0
+        BACKWARD = 2
+
+    def __init__(self, label, *largs):
+        """
+        Instantiates a new action to be applied on a presentation.
+        :param label: The label of this action.
+        :param largs: The arguments for this action. The number and types of these arguments are determined by the label.
+        """
+        super().__init__()
+        self._label = label
+        self._args = largs
+
+        if len(largs) != 0:
+            raise ValueError("The action label '{}' does not take any parameters!".format(label))
+
+    def equal(self, other):
+        if not isinstance(other, Action):
+            return False
+        return self._label == other._label and self._args == other._args
+
+    def hash(self):
+        return hash(tuple([self._label, *self._args]))
+
+    STEP = None
+
+
+Action.STEP = Action(Action.Label.FORWARD)
 
 
     STEP = None # Implement STEP!
