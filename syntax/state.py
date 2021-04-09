8274c03d281b8717bf753cfca251d000288a41fa
Gereon Fox <ger.fox07@t-online.de>
2021-04-09 18:30:47 +0200

    Stubs that will need to be filled for the behavior syntax I'm implementing.

---
+++ syntax/state.py
@@ -0,0 +1,14 @@
+
+
+class Variable:
+    """
+    A variable in the state of a process.
+    """
+    pass
+
+
+class Value:
+    """
+    A value that a process variable can be bound to.
+    """
+    pass
