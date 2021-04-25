63bbcf6969ecf2481f98e2b8803467d84c5cf54b
Gereon Fox <ger.fox07@t-online.de>
2021-04-25 17:55:07 +0200

    Create stubs for a concrete parser.

--- syntax/parsing.py
+++ syntax/parsing.py
@@ -1,4 +1,5 @@
 import abc
+from .ast import *
 
 
 class Parser(abc.ABC):
@@ -6,6 +7,7 @@ class Parser(abc.ABC):
     A parser turns sequences of tokens into abstract syntax trees.
     """
 
+    @staticmethod
     @abc.abstractmethod
     def parse_expression(self, lexer):
         """
@@ -15,6 +17,7 @@ class Parser(abc.ABC):
         """
         pass
 
+    @staticmethod
     @abc.abstractmethod
     def parse_process(self, lexer):
         """
@@ -22,4 +25,113 @@ class Parser(abc.ABC):
         :param lexer: The lexer to consume tokens from.
         :return: A Statement object.
         """
+        pass
+
+
+class SpektakelLangParser(Parser):
+    """
+    A parser for the spektakelpy default language.
+    """
+
+    @staticmethod
+    def _parse_simple_expression(lexer):
+        # Either constant, or name, or parenthesized, or tuple, or call.
+        pass
+
+    @staticmethod
+    def _parse_attribute(lexer):
+        # Attribute
+        pass
+
+    @staticmethod
+    def _parse_pow(lexer):
+
+    @staticmethod
+    def _parse_projection(lexer):
+        # Projection or simple
+        pass
+
+    @staticmethod
+    def _parse_unary(lexer):
+        # only unary minus!
+        pass
+
+    @staticmethod
+    def _parse_mult(lexer):
+        # multiplication, division, modulo.
+        pass
+
+    @staticmethod
+    def _parse_add(lexer):
+        # addition, subtraction
+        pass
+
+    @staticmethod
+    def _parse_comparison(lexer):
+        # Comparison
+        pass
+
+    @staticmethod
+    def _parse_not(lexer):
+        # boolean not.
+        pass
+
+    @staticmethod
+    def _parse_and(lexer):
+        # boolean and.
+        pass
+
+    @staticmethod
+    def _parse_or(lexer):
+        # boolean and.
+        pass
+
+
+    @staticmethod
+    def parse_expression(lexer):
+        pass
+
+    @staticmethod
+    def _parse_expression_statement(lexer):
+        pass
+
+    @staticmethod
+    def _parse_jump(lexer):
+        # return, break or continue.
+        pass
+
+    @staticmethod
+    def _parse_assignment(lexer):
+        pass
+
+    @staticmethod
+    def _parse_update(lexer):
+        pass
+
+    @staticmethod
+    def _parse_block(lexer):
+        pass
+
+    @staticmethod
+    def _parse_action_statement(lexer):
+        pass
+
+    @staticmethod
+    def _parse_when(lexer):
+        pass
+
+    @staticmethod
+    def _parse_select(lexer):
+        pass
+
+    @staticmethod
+    def _parse_while(lexer):
+        pass
+
+    @staticmethod
+    def _parse_procdef(lexer):
+        pass
+
+    @staticmethod
+    def parse_process(self, lexer):
         pass
