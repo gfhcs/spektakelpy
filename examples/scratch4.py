affdddca73ef05760791706ae786a66ffa557874
Gereon Fox <ger.fox07@t-online.de>
2021-05-23 19:56:24 +0200

    Once again changed the syntax in scratch4.py: It now adheres to the Pheno-Syntax I have in mind and it uses a pythonesque grammar.

--- examples/scratch4.py
+++ examples/scratch4.py
@@ -6,44 +6,37 @@ from alpha import presentation
 
 if __name__ == '__main__':
 
-    canvas = presentation.Canvas(1920, 1080)
+    p = presentation.Presentation(1920, 1080, fps=30)
 
-    hello = canvas.add(presentation.Text("Hello", cx=0.5, cy=0.25, visible=False))
-    world = canvas.add(presentation.Text("World", cx=0.5, cy=0.75, visible=False))
-
-    appear_soft = """
-        offer appear;
-        appear;
-        for _ in range(params.framerate):
-            wait 1 / params.framerate;
-            self.alpha += 0.1;
-    """
-
-    hello.behavior += appear_soft
-    world.behavior += appear_soft
-
-    canvas.behavior = """
-        offer forward
-            
-        def unfold():
-            forward;            
-            hello.appear;
-            forward;
-            world.appear;
-            
+    p.behavior = """
+        def appear_soft(widget):
+            for _ in range(params.framerate):
+                wait(1 / params.framerate)
+                widget.alpha += 0.1
+                
         def bounce():
             delta = 0.01
             while True:
                 for _ in range(10):
-                    wait 0.1
-                    hello.cy += delta
-                    world.cy += delta
+                    wait(0.1)
+                    self.hello.cy += delta
+                    self.world.cy += delta
                 delta = - delta   
-                
-        process unfold();
-        process bounce();         
+
+        process hello = Text("Hello")
+        process world = Text("World")
+        process _ = bounce()
+        
+        def present():
+            self.hello.position = (0.5, 0.25)
+            self.world.position = (0.5, 0.75)
+            self.hello.alpha = 0
+            self.world.alpha = 0
+            
+            forward
+            appear_soft(hello)
+            forward
+            appear_soft(world)
     """
 
-    # canvas = presentation.flatten(canvas)
-
-    presentation.present(canvas)
+    p.present()
