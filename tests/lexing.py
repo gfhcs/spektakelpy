1e571dcc01aebcc576bf9b27a450dadc256a279b
Gereon Fox <ger.fox07@t-online.de>
2021-07-22 18:51:44 +0200

    Created a test case for the lexer construction.

--- tests/lexing.py
+++ tests/lexing.py
@@ -1,7 +1,39 @@
 import unittest
+from io import StringIO
+from syntax.lexer import Lexer, PythonesqueLexicalGrammar
+
+kw_python = ["False", "await", "else", "import", "pass", "None", "break", "except", "in", "raise", "True", "class",
+             "finally", "is", "return", "and", "continue", "for", "lambda", "try", "as", "def", "from", "nonlocal",
+             "while", "assert", "del", "global", "not", "with", "async", "elif", "if", "or", "yield"]
+sep_python = ["+", "-", "*", "**", "/", "//", "%", "@", "<<", ">>", "&", "|", "^", "~", ":=", "<", ">", "<=", ">=",
+              "==", "!=", "(", ")", "[", "]", "{", "}", ",", ":", ".", ";", "@", "=", "->", "+=", "-=", "*=", "/=",
+              "//=", "%=", "@=", "&=", "|=", "^=", ">>=", "<<=", "**="]
+
+sample_python1 = """
+def perm(l):
+        # Compute the list of all permutations of l
+    if len(l) <= 1:
+                  return [l]
+    r = []
+    for i in range(len(l)):
+             s = l[:i] + l[i+1:]
+             p = perm(s)
+             for x in p:
+              r.append(l[i:i+1] + x)
+    return r
+"""
 
 
 class TestLexer(unittest.TestCase):
+
+    def test_construction(self):
+        """
+        Tests if the usual workflow of building a lexer works without crashes.
+        :return:
+        """
+        g_python = PythonesqueLexicalGrammar(kw_python, sep_python)
+        code = StringIO(sample_python1)
+        Lexer(g_python, code)
 
     def test_helloworld(self):
         s = 'hello world'
