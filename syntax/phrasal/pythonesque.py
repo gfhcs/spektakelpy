30b884b400887c08f2b53e72db3548f3e192096e
Gereon Fox <ger.fox07@t-online.de>
2021-07-27 17:19:48 +0200

    Replaced lexer.position by lexer.peek()[-1]

--- syntax/phrasal/pythonesque.py
+++ syntax/phrasal/pythonesque.py
@@ -30,7 +30,7 @@ class SpektakelLangParser(Parser):
         t, s, p = lexer.peek()
         if t == ID:
             lexer.read()
-            return Identifier(s, start=p, end=lexer.position)
+            return Identifier(s, start=p, end=lexer.peek()[-1])
         elif t == LT:
             lexer.read()
             return Constant(s, start=p, end=lexer.peek()[-1])
@@ -46,7 +46,7 @@ class SpektakelLangParser(Parser):
                 if lexer.seeing(keyword(")")):
                     if is_tuple:
                         lexer.read()
-                        return Tuple(*components, start=p, end=lexer.position)
+                        return Tuple(*components, start=p, end=lexer.peek()[-1])
                     else:
                         assert len(components) == 1
                         return components[0]
@@ -67,12 +67,12 @@ class SpektakelLangParser(Parser):
             if t == KW and s == ".":
                 lexer.read()
                 _, name, _ = lexer.match(identifier())
-                e = Attribute(e, name, start=e.start, end=lexer.position)
+                e = Attribute(e, name, start=e.start, end=lexer.peek()[-1])
             elif t == KW and s == "[":
                 lexer.read()
                 index = cls.parse_expression(lexer)
                 lexer.match(keyword("]"))
-                e = Projection(e, index, start=e.start, end=lexer.position)
+                e = Projection(e, index, start=e.start, end=lexer.peek()[-1])
             elif t == KW and s == "(":
                 lexer.read()
                 args = []
@@ -86,7 +86,7 @@ class SpektakelLangParser(Parser):
                     args.append(cls.parse_expression(lexer))
                     require_comma = True
 
-                e = Call(e, *args, start=e.start, end=lexer.position)
+                e = Call(e, *args, start=e.start, end=lexer.peek()[-1])
             else:
                 return e
 
@@ -105,7 +105,7 @@ class SpektakelLangParser(Parser):
         while t == KW and s == "**":
             lexer.read()
             e = cls._parse_application(lexer)
-            base = BinaryOperation(ArithmeticBinaryOperator.POWER, base, e, start=base.start, end=lexer.position)
+            base = BinaryOperation(ArithmeticBinaryOperator.POWER, base, e, start=base.start, end=lexer.peek()[-1])
 
         return base
 
@@ -127,7 +127,7 @@ class SpektakelLangParser(Parser):
         if positive:
             return e
         else:
-            return UnaryOperation(UnaryOperator.MINUS, e, start=e.start, end=lexer.position)
+            return UnaryOperation(UnaryOperator.MINUS, e, start=e.start, end=lexer.peek()[-1])
 
     @classmethod
     def _parse_mult(cls, lexer):
@@ -152,7 +152,7 @@ class SpektakelLangParser(Parser):
                 return base
             lexer.read()
             right = cls._parse_unary(lexer)
-            base = ArithmeticBinaryOperation(op, base, right, start=base.start, end=lexer.position)
+            base = ArithmeticBinaryOperation(op, base, right, start=base.start, end=lexer.peek()[-1])
 
     @classmethod
     def _parse_add(cls, lexer):
@@ -175,7 +175,7 @@ class SpektakelLangParser(Parser):
                 return base
             lexer.read()
             right = cls._parse_mult(lexer)
-            base = ArithmeticBinaryOperation(op, base, right, start=base.start, end=lexer.position)
+            base = ArithmeticBinaryOperation(op, base, right, start=base.start, end=lexer.peek()[-1])
 
     @classmethod
     def _parse_comparison(cls, lexer):
@@ -208,7 +208,7 @@ class SpektakelLangParser(Parser):
                 else:
                     return base
             right = cls._parse_add(lexer)
-            base = Comparison(op, base, right, start=base.start, end=lexer.position)
+            base = Comparison(op, base, right, start=base.start, end=lexer.peek()[-1])
 
     @classmethod
     def _parse_not(cls, lexer):
@@ -228,7 +228,7 @@ class SpektakelLangParser(Parser):
         if positive:
             return e
         else:
-            return UnaryOperation(UnaryOperator.NOT, e, start=e.start, end=lexer.position)
+            return UnaryOperation(UnaryOperator.NOT, e, start=e.start, end=lexer.peek()[-1])
 
     @classmethod
     def _parse_and(cls, lexer):
@@ -244,7 +244,7 @@ class SpektakelLangParser(Parser):
         while t == KW and s == "and":
             lexer.read()
             right = cls._parse_not(lexer)
-            base = BooleanBinaryOperation(BooleanBinaryOperator.AND, base, right, start=base.start, end=lexer.position)
+            base = BooleanBinaryOperation(BooleanBinaryOperator.AND, base, right, start=base.start, end=lexer.peek()[-1])
 
         return base
 
@@ -262,7 +262,7 @@ class SpektakelLangParser(Parser):
         while t == KW and s == "or":
             lexer.read()
             right = cls._parse_and(lexer)
-            base = BooleanBinaryOperation(BooleanBinaryOperator.OR, base, right, start=base.start, end=lexer.position)
+            base = BooleanBinaryOperation(BooleanBinaryOperator.OR, base, right, start=base.start, end=lexer.peek()[-1])
 
         return base
 
@@ -304,23 +304,23 @@ class SpektakelLangParser(Parser):
             if len(es) == 1:
                 e = es
             else:
-                e = Tuple(*es, start=es[0].start, end=lexer.position)
+                e = Tuple(*es, start=es[0].start, end=lexer.peek()[-1])
 
             if t == KW and s == ":":
                 if not isinstance(e, Identifier):
-                    raise ParserError("Only an identifier is allowed on the left hand side of ':'!", pos=lexer.position)
+                    raise ParserError("Only an identifier is allowed on the left hand side of ':'!", pos=lexer.peek()[-1])
                 lexer.read()
                 s = cls._parse_statement(lexer)
-                return ActionStatement(e, s, start=e.start, end=lexer.position)
+                return ActionStatement(e, s, start=e.start, end=lexer.peek()[-1])
             elif t == KW and s == "=":
                 if not isinstance(e, AssignableExpression):
                     raise ParserError("Only 'assignable' expressions must occuron the left hand side of an assignment!",
-                                      pos=lexer.position)
+                                      pos=lexer.peek()[-1])
                 lexer.read()
                 right = cls.parse_expression(lexer)
-                return Assignment(e, right, start=e.start, end=lexer.position)
+                return Assignment(e, right, start=e.start, end=lexer.peek()[-1])
             else:
-                return ExpressionStatement(e, start=e.start, end=lexer.position)
+                return ExpressionStatement(e, start=e.start, end=lexer.peek()[-1])
 
         return sub_parser(lexer)
 
@@ -333,7 +333,7 @@ class SpektakelLangParser(Parser):
         """
         _, _, p = lexer.match(keyword("return"))
         e = cls.parse_expression(lexer)
-        return Return(e, start=p, end=lexer.position)
+        return Return(e, start=p, end=lexer.peek()[-1])
 
     @classmethod
     def _parse_continue(cls, lexer):
@@ -343,7 +343,7 @@ class SpektakelLangParser(Parser):
         :return: A Continue node.
         """
         _, _, p = lexer.match(keyword("continue"))
-        return Continue(start=p, end=lexer.position)
+        return Continue(start=p, end=lexer.peek()[-1])
 
     @classmethod
     def _parse_break(cls, lexer):
@@ -353,7 +353,7 @@ class SpektakelLangParser(Parser):
         :return: A Break node.
         """
         _, _, p = lexer.match(keyword("break"))
-        return Break(start=p, end=lexer.position)
+        return Break(start=p, end=lexer.peek()[-1])
 
     @classmethod
     def _parse_statements(cls, lexer, t):
@@ -379,7 +379,7 @@ class SpektakelLangParser(Parser):
         _, _, start = lexer.match(keyword("{"))
         statements = cls._parse_statements(lexer, keyword("}"))
         lexer.match(keyword("}"))
-        return Block(statements, start=start, end=lexer.position)
+        return Block(statements, start=start, end=lexer.peek()[-1])
 
     @classmethod
     def _parse_when(cls, lexer):
@@ -395,7 +395,7 @@ class SpektakelLangParser(Parser):
         lexer.match(keyword(")"))
         statement = cls._parse_statement(lexer)
 
-        return When(condition, statement, start=start, end=lexer.position)
+        return When(condition, statement, start=start, end=lexer.peek()[-1])
 
     @classmethod
     def _parse_select(cls, lexer):
@@ -420,7 +420,7 @@ class SpektakelLangParser(Parser):
 
             ss = cls._parse_statements(lexer, t)
             if len(ss) != 1:
-                s = Block(ss, start=sstart, end=lexer.position)
+                s = Block(ss, start=sstart, end=lexer.peek()[-1])
             else:
                 s, = ss
 
@@ -428,7 +428,7 @@ class SpektakelLangParser(Parser):
 
         lexer.match(keyword("}"))
 
-        return Select(alternatives, start=start, end=lexer.position)
+        return Select(alternatives, start=start, end=lexer.peek()[-1])
 
     @classmethod
     def _parse_if(cls, lexer):
@@ -455,7 +455,7 @@ class SpektakelLangParser(Parser):
             When(condition, consequence, start=start, end=consequence.end),
             When(UnaryOperation(UnaryOperator.NOT, condition, start=alternative.start, end=alternative.start),
                  alternative, start=alternative.start, end=alternative.end)
-        ], start=start, end=lexer.position)
+        ], start=start, end=lexer.peek()[-1])
 
     @classmethod
     def _parse_while(cls, lexer):
@@ -471,7 +471,7 @@ class SpektakelLangParser(Parser):
         lexer.match(keyword(")"))
         body = cls._parse_statement(lexer)
 
-        return While(condition, body, start=start, end=lexer.position)
+        return While(condition, body, start=start, end=lexer.peek()[-1])
 
     @classmethod
     def _parse_procdef(cls, lexer):
