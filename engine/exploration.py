59f0ca52e0a2d5d865718caaf56fa19b519e8e17
Gereon Fox <ger.fox07@t-online.de>
2022-02-06 12:20:23 +0100

    Implemented engine.exploration.state_space.

--- engine/exploration.py
+++ engine/exploration.py
@@ -1,5 +1,6 @@
 from util import check_type
 from .machine import MachineState
+from util.lts import LTS, State, Transition
 
 
 def explore(mstate, scheduler=None):
@@ -10,6 +11,7 @@ def explore(mstate, scheduler=None):
     which Tasks are eligible for being scheduled in state s. By default, *all* tasks are eligible in all states.
     :return: An iterable of tuples (s, es), where es is an iterable of pairs (tid, s'), where tid is a task ID object
     specifying a task execution of which transforms s into s'. es comprises *all* pairs with this property.
+    The very first s enumerated by this method will be the initial state. es may be empty.
     """
 
     check_type(mstate, MachineState)
@@ -38,9 +40,37 @@ def explore(mstate, scheduler=None):
 def state_space(transitions):
     """
     Assembles a set of transitions into a labelled-transition-system.
-    :param transitions: An iterable of tuples (s, es), where es is an iterable of pairs (t, s'), where t is a TaskState
-    that is part of MachineState s, execution of which leads to MachineState s'. es comprises *all* pairs with this
-    property.
-    :return: An LTS object.
+    :param transitions: An iterable of tuples (s, es), where es is an iterable of pairs (tid, s'), where tid is a task
+    ID object specifying a task execution of which transforms s into s'. es comprises *all* pairs with this property.
+    :return: An LTS object. The initial state of this LTS will be the origin of the very first transition enumerated
+    in 'transitions'.
     """
-    raise NotImplementedError()
+
+    states = {}
+    s0 = None
+
+    for s, es in transitions:
+        try:
+            origin = states[s]
+        except KeyError:
+            origin = State(s)
+            states[s] = origin
+            if s0 is None:
+                s0 = origin
+
+        for tid, t in es:
+            try:
+                destination = states[t]
+            except KeyError:
+                destination = State(t)
+                states[t] = destination
+
+            origin.add_transition(Transition(tid, destination))
+
+    # We're doing it here because not all states might be enumerated as origins (i.e. if they don't have outgoing
+    # transitions).
+    for s in states.values():
+        s.seal()
+
+    assert s0 is not None
+    return LTS(s0)
