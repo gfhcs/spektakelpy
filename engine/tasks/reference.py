dd23d14c5727255a4c7c0e9799a3f574d9089bf8
Gereon Fox <ger.fox07@t-online.de>
2022-02-19 19:14:53 +0100

    Fully implemented the abstract 'Reference' type.

--- engine/tasks/reference.py
+++ engine/tasks/reference.py
@@ -1,11 +1,32 @@
 import abc
 from util.immutable import Sealable
 
-class Reference(Sealable):
+
+class Reference(Sealable, abc.ABC):
     """
-    A reference points to a part of a machine state.
+    A reference is a part of a machine state that can point to another part of a machine state.
     """
 
-    def write(self, mstate, value):
+    @abc.abstractmethod
+    def write(self, tstate, mstate, value):
+        """
+        Updates the value stored at the location that this reference is pointing to.
+        :param tstate: The TaskState in the context of which this reference is to be interpreted. It must be part
+                       of the given mstate.
+        :param mstate: The MachineState in the context of which this reference is to be interpreted. It must contain
+                       tstate.
+        :param value: The value to store at the location that this reference is pointing to.
+        """
+        pass
 
-    def read(self, mstate):
+    @abc.abstractmethod
+    def read(self, tstate, mstate):
+        """
+        Obtains the value that this reference is pointing to.
+        :param tstate: The TaskState in the context of which this reference is to be interpreted. It must be part
+                       of the given mstate.
+        :param mstate: The MachineState in the context of which this reference is to be interpreted. It must contain
+                       tstate.
+        :return: The object pointed to by this reference.
+        """
+        pass
