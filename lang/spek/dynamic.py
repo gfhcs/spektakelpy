97e434e8da527847800681ceebe537bf93fc00bc
Gereon Fox <ger.fox07@t-online.de>
2022-03-15 21:48:21 +0100

    Fully implemented the Chain class, including Chain.compile.

--- lang/spek/dynamic.py
+++ lang/spek/dynamic.py
@@ -4,6 +4,7 @@ from .ast import Pass, Constant, Identifier, Attribute, Tuple, Projection, Call,
     ExpressionStatement, Assignment, Block, Return, Raise, Break, \
     Continue, Conditional, While, For, Try, VariableDeclaration, ProcedureDefinition, \
     PropertyDefinition, ClassDefinition
+from engine.tasks.instructions import Push, Pop, Launch, Update, Guard, StackProgram
 
 
 class Chain:
@@ -11,7 +12,15 @@ class Chain:
     Represents a sequence of instructions. Control flow can enter this chain only at its start.
     """
     def __init__(self):
-        pass
+        self._proto = []
+        self._targets = set()
+        self._can_continue = True
+
+    def _assert_continuable(self):
+        if self._proto is None:
+            raise RuntimeError("This chain has been finalized and cannot be modified anymore!")
+        if not self._can_continue:
+            raise RuntimeError("This chain cannot be extended, because of the type of its last instruction!")
 
     def append_update(self, ref, expression, on_error):
         """
@@ -20,7 +29,9 @@ class Chain:
         :param expression: The Expression object specifying how to compute the new value.
         :param on_error: The chain to jump to if the instruction causes an error.
         """
-        pass
+        self._assert_continuable()
+        self._proto.append((Update, ref, expression, on_error))
+        self._targets.add(on_error)
 
     def append_guard(self, alternatives, on_error):
         """
@@ -30,7 +41,12 @@ class Chain:
                              condition.
         :param on_error: The chain to jump to in case the instruction causes an error.
         """
-        pass
+        self._assert_continuable()
+        self._proto.append((Guard, alternatives, on_error))
+        for _, t in alternatives.items():
+            self._targets.add(t)
+        self._targets.add(on_error)
+        self._can_continue = False
 
     def append_push(self, entry, aexpressions, on_error):
         """
@@ -43,13 +59,18 @@ class Chain:
                          lead to this error destination! To handle those errors, instructions following the push
                          instruction must explicitly treat them!
         """
-        pass
+        self._assert_continuable()
+        self._proto.append((Push, entry, aexpressions, on_error))
+        self._targets.add(on_error)
 
     def append_pop(self):
         """
         Appends a prototype of a Pop instruction to this chain.
+        The chain cannot be continued after a pop instruction.
         """
-        pass
+        self._assert_continuable()
+        self._proto.append((Pop, ))
+        self._can_continue = False
 
     def append_launch(self, entry, aexpressions, on_error):
         """
@@ -62,8 +83,56 @@ class Chain:
                          lead to this error destination! To handle those errors, instructions following the push
                          instruction must explicitly treat them!
         """
-        pass
+        self._assert_continuable()
+        self._proto.append((Launch, entry, aexpressions, on_error))
+        self._targets.add(on_error)
 
+    def compile(self):
+        """
+        Compiles this chain and the chains it may jump to into a StackProgram.
+        :return: A StackProgram object.
+        """
+
+        offset = 0
+        entries = {}
+        chains = [self]
+
+        while len(chains) > 0:
+            c = chains.pop()
+            if c in entries:
+                continue
+            if c._can_continue:
+                raise RuntimeError("Cannot compile chains that do not end with either a guard or a pop instruction!")
+            entries[c] = offset
+            offset += len(c)
+            chains.extend(c._targets)
+
+        instructions = []
+        offset = 0
+
+        for c in entries.keys(): # Enumerates the chains in the order they were inserted, guaranteeing that they start
+                                 # exactly at the recorded offsets.
+            for t, *args in c._proto:
+                if t is Update:
+                    ref, expression, on_error = args
+                    instructions.append(Update(ref, expression, offset + 1, entries[on_error]))
+                elif t is Guard:
+                    alternatives, on_error = args
+                    instructions.append(Guard({condition: entries[chain] for condition, chain in alternatives.items()}, entries[on_error]))
+                elif t is Push:
+                    entry, expressions, on_error = args
+                    instructions.append(Push(entry, expressions, offset + 1, entries[on_error]))
+                elif t is Pop:
+                    instructions.append(Pop())
+                elif t is Launch:
+                    entry, expressions, on_error = args
+                    instructions.append(Launch(entry, expressions, offset + 1, entries[on_error]))
+                else:
+                    raise NotImplementedError("Bug in Chain.compile: The instruction type {} "
+                                              "has not been taken into account for compilation yet!".format(t))
+                offset += 1
+
+        return StackProgram(instructions)
 
 class Spektakel2Stack(Translator):
     """
