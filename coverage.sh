287c60701432efe29b1f5aa898fc862f0a46a225
Gereon Fox <ger.fox07@t-online.de>
2023-12-14 23:20:05 +0800

    Added the printing tests to the coverage suite.

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-coverage run --branch -m unittest tests.lexing tests.parsing tests.validation tests.machine tests.translation
+coverage run --branch -m unittest tests.lexing tests.parsing tests.validation tests.machine tests.translation tests.printing
 coverage report
 coverage html
