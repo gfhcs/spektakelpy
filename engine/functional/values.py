dd8c4f78554aee94960616eab8ad2e04380491ab
Gereon Fox <ger.fox07@t-online.de>
2024-02-04 17:15:29 +0100

    Added VCell.

--- engine/functional/values.py
+++ engine/functional/values.py
@@ -1,5 +1,6 @@
 from engine.intrinsic import IntrinsicInstanceMethod
 from util import check_type
+from util.immutable import check_unsealed
 from . import Value
 
 
@@ -398,6 +399,65 @@ class VStr(Value):
 
     def __ge__(self, other):
         return VBool.from_bool(self._value >= other._value)
+
+
+class VCell(Value):
+    """
+    An object that references another object.
+    """
+
+    def __init__(self, ref):
+        """
+        Creates a new cell.
+        :param ref: The object this cell should contain.
+        """
+        super().__init__()
+        self._ref = check_type(ref, Value)
+
+    @property
+    def contained(self):
+        """
+        The object contained in this cell.
+        :return: A Value.
+        """
+        return self._ref
+
+    @contained.setter
+    def contained(self, value):
+        check_unsealed(self)
+        self._ref = check_type(value, Value)
+
+    def print(self, out):
+        out.write("Cell(")
+        self._ref.print(out)
+        out.write(")")
+
+    def __repr__(self):
+        return f"VCell({repr(self._ref)})"
+
+    @property
+    def type(self):
+        return TBuiltin.cell
+
+    def hash(self):
+        return hash(self._ref) ^ 47
+
+    def equals(self, other):
+        return isinstance(other, VCell) and self._ref == other._ref
+
+    def _seal(self):
+        self._ref.seal()
+
+    def clone_unsealed(self, clones=None):
+        if clones is None:
+            clones = {}
+        try:
+            return clones[id(self)]
+        except KeyError:
+            c = VCell(self._ref)
+            clones[id(self)] = c
+            c._ref = self._ref.clone_unsealed(clones=clones)
+            return c
 
 
 class VTuple(Value):
