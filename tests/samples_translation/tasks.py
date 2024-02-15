b509dabcd685258854ced96d29b9a02fbcc34770
Gereon Fox <ger.fox07@t-online.de>
2024-02-15 19:01:08 +0100

    Another concurrency test case, including futures.

--- tests/samples_translation/tasks.py
+++ tests/samples_translation/tasks.py
@@ -22,5 +22,47 @@ result = await task
 await never()
 """: ((4, 3, 3), {"result": 42, "done": True}),
 
+"""
+from interaction import next
+
+var fbe = None
+def buffer_empty():
+    if fbe is None or fbe.done:
+        fbe = future()
+    return fbe
+    
+var fbf = None
+def buffer_full():
+    if fbf is None or fbf.done:
+        fbf = future()
+    return fbf
+
+var buffer = None
+
+def produce():
+    acc = 123
+    while acc > 0:
+        await next()
+        await buffer_empty()
+        buffer = acc % 10
+        buffer_full().set(True)
+        acc = acc // 10
+    
+var consumed = 0
+def consume():
+    while True:    
+        await buffer_nonempty()
+        consumed = 10 * consumed + buffer % 10
+        buffer = None
+        buffer_empty().set(True)
+        
+var c = async consume()
+var p = async produce()
+
+# The following never terminate:
+await p
+await c
+
+""": ((11, 9, 6), {"consumed": 321, "buffer": None}),
 
 }
