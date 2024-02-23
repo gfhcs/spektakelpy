359deb97253e984bb7cddce03f8869c5e650e86f
Gereon Fox <ger.fox07@t-online.de>
2024-02-23 14:26:44 +0100

    Adjusted the diamond sample, such that we can label states properly for bisimilarity checking.

--- tests/samples_translation/diamond.py
+++ tests/samples_translation/diamond.py
@@ -2,14 +2,17 @@ code = """
 from interaction import next, prev
 
 var n = 2
+var state = 0
 
-def count(n, i):
+def count(n, i, sidx):
     var c = 0
     while True:
         await i()
+        state = state - ((state // 10 ** sidx) % 10) * 10 ** sidx
         c = (c + 1) % n
+        state = state + c * 10 ** sidx
 
-var c1, c2 = async count(n, next), async count(n, prev)
+var c1, c2 = async count(n, next, 1), async count(n, prev, 0)
 
 # These never terminate:
 await c1
