ce46047ccd0b75db87a9febd96507804f59ce8d8
Gereon Fox <ger.fox07@t-online.de>
2024-03-04 08:59:48 +0100

    Minor fix for the builtin isinstance operator.

--- engine/functional/procedures.py
+++ engine/functional/procedures.py
@@ -73,6 +73,8 @@ def builtin(name):
 
 @builtin("isinstance")
 def builtin_isinstance(tstate, mstate, x, types):
+    if not isinstance(types, tuple):
+        types = (types, )
     t = x.type
     return VBool.from_bool(any(t.subtypeof(s) for s in types))
 
